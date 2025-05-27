9f3821e46f342eabbdc7a406e77ba487e1e7dab9
carryingtheflame <h.mustafa.mail@gmail.com>
2025-05-27 11:04:10 -0700

    restored exceptional behavior

--- ValidateAndTime.py
+++ ValidateAndTime.py
@@ -11,34 +11,34 @@ results = []
 for fname in os.listdir(DATA_DIR):
     fpath = os.path.join(DATA_DIR, fname)
     
-    #try:
-    data = np.loadtxt(fpath)
-    N = data.shape[0]
-    
-    # Statsmodels docs says implementation can't handle large arrays
-    if N > 55000:
-        continue
+    try:
+        data = np.loadtxt(fpath)
+        N = data.shape[0]
+        
+        # Statsmodels docs says implementation can't handle large arrays
+        if N > 55000:
+            continue
 
-    print(f"processing {fname}...")
+        print(f"processing {fname}...")
 
-    start = time.perf_counter()
-    my_mc = medcouple(data, use_fast=True)
-    my_time = time.perf_counter() - start
+        start = time.perf_counter()
+        my_mc = medcouple(data, use_fast=True)
+        my_time = time.perf_counter() - start
 
-    start = time.perf_counter()
-    sm_mc = medcouple(data, use_fast=False)
-    sm_time = time.perf_counter() - start
+        start = time.perf_counter()
+        sm_mc = medcouple(data, use_fast=False)
+        sm_time = time.perf_counter() - start
 
-    results.append({
-        "filename": fname,
-        "N": N,
-        "custom_medcouple": my_mc,
-        "statsmodels_medcouple": sm_mc,
-        "my_time": my_time,
-        "sm_time": sm_time
-    })
+        results.append({
+            "filename": fname,
+            "N": N,
+            "custom_medcouple": my_mc,
+            "statsmodels_medcouple": sm_mc,
+            "my_time": my_time,
+            "sm_time": sm_time
+        })
 
-    '''except Exception as e:
+    except Exception as e:
         print(f"Failed to process {fname}: {e}")
         results.append({
             "filename": fname,
@@ -47,7 +47,7 @@ for fname in os.listdir(DATA_DIR):
             "statsmodels_medcouple": "error",
             "my_time": None,
             "sm_time": None
-        })'''
+        })
 
 with open(OUTPUT_CSV, mode="w", newline="") as csvfile:
     fieldnames = ["filename", "N", "custom_medcouple", "statsmodels_medcouple", "my_time", "sm_time"]
